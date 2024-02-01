c4a998fa3ee0e8e748caa4e74c5f9bb64f435d34
MicheleVicidomini <michele.vicidomini@intecs.it>
2024-02-01 16:24:52 +0100

    Prima implentazione.

---
+++ src/main/java/com/example/resttemplateprojects/RestTemplateUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.resttemplateprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTemplateUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestTemplateUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/resttemplateprojects/RestUtenteApplicationTest.java
@@ -0,0 +1,37 @@
+package com.example.resttemplateprojects;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import utente.Utente;
+
+
+public class RestUtenteApplicationTest {
+
+    @Test
+    void config() {
+
+        RestTemplate restTemplate = new RestTemplate(); // abbiamo richiamato l'istanza dell'oggetto RestTemplate
+        String fooResourceUrl =
+                "https://65bb5fb252189914b5bbe4cf.mockapi.io/api/v1/users";
+        HttpEntity<Utente> request = new HttpEntity<>(new Utente("Luigi", "Frattamaggiore", "Italia", "f", "2"));
+        Utente utente = restTemplate.postForObject(fooResourceUrl, request, Utente.class);
+        Assertions.assertNotNull(utente);
+    }
+
+    @Test
+    void restTemp() {
+
+        RestTemplate restTemplate = new RestTemplate(); // abbiamo richiamato l'istanza dell'oggetto RestTemplate
+        String fooResourceUrl =
+                "https://65bb5fb252189914b5bbe4cf.mockapi.io/api/v1/users";
+        ResponseEntity<String> response =
+                restTemplate.getForEntity(fooResourceUrl + "/1", String.class);
+        Assertions.assertEquals(response.getStatusCode(), HttpStatus.OK);
+
+    }
+}
