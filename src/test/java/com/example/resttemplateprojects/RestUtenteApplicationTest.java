788c5c26f73d7826f9b3272fc43b9b6ef554d75d
MicheleVicidomini <michele.vicidomini@intecs.it>
2024-02-02 09:35:44 +0100

    Prima imreplentazione

--- src/test/java/com/example/resttemplateprojects/RestUtenteApplicationTest.java
+++ src/test/java/com/example/resttemplateprojects/RestUtenteApplicationTest.java
@@ -13,8 +13,7 @@ import utente.Utente;
 public class RestUtenteApplicationTest {
 
     @Test
-    void configg()
-    {
+    void config() {
 
         RestTemplate restTemplate = new RestTemplate(); // abbiamo richiamato l'istanza dell'oggetto RestTemplate
         String fooResourceUrl =
